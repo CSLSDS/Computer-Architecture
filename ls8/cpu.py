1f47d7356e931fc32cdc324f06e9ffd7e8da359b
cslsds <lambdadatasciencelearning@gmail.com>
2020-08-18 16:30:44 -0400

    mod1 mvp complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,13 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.pc = 0
+        self.reg = [0] * 8
+        self.op_size = 1
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.HLT = 0b00000001
 
     def load(self):
         """Load a program into memory."""
@@ -37,6 +43,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -60,6 +68,28 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            cmd = self.ram_read(self.pc)
+            
+            if cmd == self.LDI:
+                regix = self.ram[self.pc + 1]
+                numreg = self.ram[self.pc + 2]
+                self.reg[regix] = numreg
+                self.op_size = 3
+
+            elif cmd == self.PRN:
+                regix = self.ram[self.pc + 1]
+                print(self.reg[regix])
+                self.op_size = 2
+
+            elif cmd == self.HLT:
+                running = False
+                self.op_size = 1
+
+            self.pc += self.op_size
